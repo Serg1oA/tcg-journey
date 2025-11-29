6a1f95077370675a5da458d290518e21d21d21b8
Sérgio Almeida <almeida.sergiomc@gmail.com>
2025-11-29 21:51:46 +0000

    Setup of expo project

---
+++ tcg-journey/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Sticker Smash"
+        }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{
+          headerTitle: "About Screen"
+        }}
+      />
+    </Stack>
+  );
+}
---
+++ tcg-journey/app/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
+      <Link href={"/about"} style={styles.button}>
+        Go to About Screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#131313ff"
+  },
+  text: {
+    color: "white"
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "white"
+  }
+});
